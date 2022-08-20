549c75b513150d5e13f3f614783aafc220719af9
MrChelizt <chelizt@gmail.com>
2022-08-20 18:32:58 +0200

    Add Shoe detail screen

--- starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment : Fragment() {
 
@@ -12,7 +15,11 @@ class InstructionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val binding: FragmentInstructionsBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        binding.toShoeListButton.setOnClickListener { v: View ->
+            v.findNavController().navigate(InstructionsFragmentDirections.actionInstructionsFragmentToShoeListFragment())
+        }
+        return binding.root
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -2,7 +2,10 @@ package com.udacity.shoestore
 
 import android.os.Bundle
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
 class ShoeListFragment : Fragment() {
 
@@ -10,8 +13,14 @@ class ShoeListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_list, container, false)
+        val binding: FragmentShoeListBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_shoe_list, container, false
+        )
+        binding.addShoeButton.setOnClickListener { v: View ->
+            v.findNavController()
+                .navigate(ShoeListFragmentDirections.actionShoeListFragmentToShoeDetailFragment())
+        }
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
